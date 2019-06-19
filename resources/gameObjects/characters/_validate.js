e4bd02ee126e4f96ffe4b8ef1bb739a345235b44
Jack Frost <j4qfrost@gmail.com>
2019-06-19 03:39:13 -0700

    delete cache for assets

--- resources/gameObjects/characters/_validate.js
+++ resources/gameObjects/characters/_validate.js
@@ -3,11 +3,14 @@ const path = require('path');
 const assetType = 'GameObject';
 
 exports.validateAssets = async(validator) => {
+    const cachedPaths = [];
     fs.readdir(__dirname, (err, files) => {
         console.log('Validating Character assets:');
         files.forEach(file => {
             if (file[0] !== '_' && file[0] !== '.' && file.slice(-5) === '.json') {
-                const object = require(path.join(__dirname, file));
+                const fullFilePath = path.join(__dirname, file);
+                cachedPaths.push(fullFilePath);
+                const object = require(fullFilePath);
                 const result = validator.validate(object, `/${assetType}`);
                 if (result.valid) {
                     console.log(file, 'is valid.');
@@ -16,5 +19,8 @@ exports.validateAssets = async(validator) => {
                 }
             }
         });
+    });
+    cachedPaths.forEach(cachedPath => {
+        delete require.cache[require.resolve(cachedPath)];
     });
 }
